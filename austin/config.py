4e63b605fa0189e91e64e4fc7729f9c32b54b139
abalarin <abalarin@outlook.com>
2019-03-02 14:18:21 -0500

    psql

--- austin/config.py
+++ austin/config.py
@@ -10,8 +10,5 @@ class Config():
     # Connection to Postgres server
     SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@45.33.79.194:5432/austinbalarin'
 
-    # Connection to local postgres db
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://abalarin:lindoe!@localhost:5432/austinbalarin'
-
     # To suppress FSADeprecationWarning
     SQLALCHEMY_TRACK_MODIFICATIONS = False
