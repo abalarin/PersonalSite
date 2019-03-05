7f5469969e3be892cf6bd75ce8d9411f5a254b5f
abalarin <abalarin@outlook.com>
2019-03-05 00:25:32 -0500

    embed videos

---
+++ austin/static/scripts/index.js
@@ -0,0 +1,10 @@
+// var figure = $(".video").hover( hoverVideo, hideVideo );
+//
+// function hoverVideo() {
+//     $('video', this).get(0).play();
+// }
+//
+// function hideVideo() {
+//     $('video', this).get(0).pause();
+// }
+console.log("print")
