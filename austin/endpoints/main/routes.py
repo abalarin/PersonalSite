bdfd11c1a8ea1f913c0821b533f97c1c9901ed72
abalarin <abalarin@outlook.com>
2019-03-08 13:16:36 -0500

    fix github links

--- austin/endpoints/main/routes.py
+++ austin/endpoints/main/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 import requests
+import datetime
+from dateutil import parser
 
 from austin.endpoints.gallery.routes import *
 from austin import Config
@@ -14,16 +16,25 @@ def index():
 
 @main.route('/changelog')
 def change_log():
-    github_feed = requests.get('https://api.github.com/users/abalarin/events/public?access_token=' + Config.GITHUB_TOKEN).json()
+    github_feed = requests.get(
+        'https://api.github.com/users/abalarin/events/public?access_token=' + Config.GITHUB_TOKEN).json()
     return render_template('changelog/github.html', changelog=github_feed)
 
+# Return 5 of the most recent Github Actions
 
-@main.route('/changelog1')
+
 def github_feed():
+    github_feed = requests.get(
+        'https://api.github.com/users/abalarin/events/public?access_token=' + Config.GITHUB_TOKEN + '&per_page=5').json()
 
-    github_feed = requests.get('https://api.github.com/users/abalarin/events/public?access_token=' + Config.GITHUB_TOKEN).json()
-    
     return github_feed
+
+# This is too slow - optimize
+@main.context_processor
+def jinja_api_caller():
+    def get_json_from(url):
+        return requests.get(url + '?access_token=' + Config.GITHUB_TOKEN).json()
+    return dict(get_json_from=get_json_from)
 
 
 @main.app_errorhandler(401)
