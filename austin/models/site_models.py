b5d0dcb6ef0109086cf7fa9811143f1b92f8a6d1
abalarin <abalarin@outlook.com>
2019-03-09 11:48:26 -0500

    spotify refactor

--- austin/models/site_models.py
+++ austin/models/site_models.py
@@ -1,10 +1,12 @@
-from sqlalchemy.sql import func
 from austin import db
+
 
 class Configuration(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     spotify_code = db.Column(db.String(256))
+    spotify_access_token = db.Column(db.String(256))
+    spotify_refresh_token = db.Column(db.String(256))
 
     def __repr__(self):
         return(str(self.id) + ", " + self.name)
