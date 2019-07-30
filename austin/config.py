9ebb0f693178f612990f628387c5cdfb9656730a
abalarin <abalarin@outlook.com>
2019-07-30 10:01:09 -0400

    key refactor

--- austin/config.py
+++ austin/config.py
@@ -1,13 +1,18 @@
 import os
 
+import json
+import urllib3
+
+with open(os.path.dirname(os.path.abspath(__file__)) + '/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config():
 
     SECRET_KEY = os.urandom(12)
 
     # Postgres keys
-    DB_USER = os.environ.get('DB_USER')
-    DB_PASS = os.environ.get('DB_PASS')
+    DB_USER = config.get('DB_USER')
+    DB_PASS = config.get('DB_PASS')
 
     # Connection to Postgres server
     SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@45.33.79.194:5432/austinbalarin'
@@ -16,16 +21,16 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     #  boto3 Keys for Object Storage
-    BOTO_KEY = os.environ.get('BOTO_KEY')
-    BOTO_SECRET = os.environ.get('BOTO_SECRET')
+    BOTO_KEY = config.get('BOTO_KEY')
+    BOTO_SECRET = config.get('BOTO_SECRET')
 
     # Github OAuth
-    GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+    GITHUB_TOKEN = config.get('GITHUB_TOKEN')
 
     # Gets pwd and declares it is the root dir for the App
     APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     # Spotify Stuff
-    SPOTIFY_REDIRECT = os.environ.get('SPOTIFY_REDIRECT')
-    SPOTIFY_ID = os.environ.get('SPOTIFY_ID')
-    SPOTIFY_SECRET = os.environ.get('SPOTIFY_SECRET')
+    SPOTIFY_REDIRECT = config.get('SPOTIFY_REDIRECT')
+    SPOTIFY_ID = config.get('SPOTIFY_ID')
+    SPOTIFY_SECRET = config.get('SPOTIFY_SECRET')
