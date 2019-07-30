b60da0fb6c4151e6ae8f70e95519319ace4ee33f
abalarin <abalarin@outlook.com>
2019-07-30 10:17:19 -0400

    disable warnings

--- austin/config.py
+++ austin/config.py
@@ -2,6 +2,7 @@ import os
 
 import json
 import urllib3
+urllib3.disable_warnings()
 
 with open(os.path.dirname(os.path.abspath(__file__)) + '/config.json') as config_file:
     config = json.load(config_file)
